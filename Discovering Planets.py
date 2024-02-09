8b29e143520c0e1b0ab710e3876a060143fa6d1b
ArthurTheAardvar <114256031+ArthurTheAardvar@users.noreply.github.com>
2024-02-09 15:25:51 -0700

    Update Discovering Planets.py

--- Discovering Planets.py
+++ Discovering Planets.py
@@ -9,19 +9,19 @@ for i in range(cases): #go through the other lines
     
     
     if float(line[0]) > 100:
-        print("The planet is too hot")
+        print("The planet is too hot.")
 
     elif float(line[0]) < 0:
-        print("The planet is too cold")
+        print("The planet is too cold.")
 
     if str(line[1]) == "false" and float(line[3]) < 0.6:
-        print("The planet has no water")
+        print("The planet has no water.")
 
     elif str(line[2]) == "false":
-        print("The planet has no magnetic field")
+        print("The planet has no magnetic field.")
 
     elif float(line[3]) > 0.6 and float(line[0]) > 0:
-        print("The planet's orbit is not ideal")
+        print("The planet's orbit is not ideal.")
 
     if float(line[0]) < 100 and float(line[0]) > 0 and str(line[1]) == "true" and str(line[2]) == "true" and float(line[3]) < 0.6:
-        print("The planet is habitable")
+        print("The planet is habitable.")
